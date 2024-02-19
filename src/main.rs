513f4c85ea4871a68b62d4911962ca89f1dc4ac8
Ismael Trentin <ismael.trentin@samtrevano.ch>
2024-02-19 20:21:21 +0100

    feat: windows compat and text styling

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use colored::Colorize;
 use error::CliError;
 use std::fs;
 use std::io;
-use std::os::unix::fs::MetadataExt;
 use std::process::Command;
+
+#[cfg(target_os = "linux")]
+use std::os::unix::fs::MetadataExt;
+
+#[cfg(target_os = "windows")]
+use std::os::windows::fs::MetadataExt;
 
 pub mod error;
 
@@ -44,28 +49,30 @@ fn get_asd_files_paths(root_dir: &str) -> Result<Vec<String>, std::io::Error> {
     let output;
 
     if cfg!(target_os = "windows") {
-        return Err(std::io::Error::new(
-            io::ErrorKind::Unsupported,
-            CliError::UnsupportedOS,
-        ));
-    } // else
-    output = Command::new("find")
-        .arg(root_dir)
-        .arg("-name")
-        .arg("*.asd")
-        .output()?;
+        let mut path = String::from(root_dir);
+        path.extend("\\*.asd".chars());
+
+        let mut cmd = String::from("dir ");
+        cmd.extend(path.chars());
+        cmd.extend(" /o /s /b".chars());
+
+        output = Command::new("cmd").args(&["/C", &cmd]).output()?;
+    } else {
+        output = Command::new("find")
+            .args(&[root_dir, "-name", "*.asd"])
+            .output()?;
+    }
 
     if let Some(exit_code) = output.status.code() {
         if exit_code != 0 {
             return Err(std::io::Error::new(
                 io::ErrorKind::Other,
-                String::from_utf8(output.stderr).unwrap_or("find command failed".to_owned()),
+                String::from_utf8_lossy(&output.stderr),
             ));
         }
     }
 
-    let stdout = String::from_utf8(output.stdout)
-        .map_err(|e| std::io::Error::new(io::ErrorKind::InvalidData, e))?;
+    let stdout = String::from_utf8_lossy(&output.stdout);
     let lines = stdout.lines().map(|l| l.to_owned()).collect();
 
     Ok(lines)
@@ -75,7 +82,7 @@ fn main() -> Result<(), CliError> {
     let args = CliArgs::parse();
 
     if args.force {
-        println!("WARN: running in force mode\n");
+        println!("🚨 running in {} mode\n", "FORCE".red());
     }
 
     if args.dry_run {
@@ -83,7 +90,10 @@ fn main() -> Result<(), CliError> {
     }
 
     if args.purge && !args.force && !args.dry_run {
-        println!("purge mode will delete files. continue? (Y/n) ");
+        println!(
+            "⚠️ purge mode will delete files. {}? (Y/n) ",
+            "continue".yellow()
+        );
 
         let stdin = io::stdin();
         let input = &mut String::new();
@@ -96,45 +106,65 @@ fn main() -> Result<(), CliError> {
         }
     }
 
-    println!("root dir: {}", &args.root_node);
+    println!("🔍 searching into: {}", &args.root_node.cyan());
 
     let files = get_asd_files_paths(&args.root_node)?;
     let mut sizes = Vec::with_capacity(files.len());
 
-    for file in files {
-        if let Ok(meta) = fs::metadata(&file) {
-            sizes.push(meta.size());
-        } else {
-            println!("{} {}", "could not read metadata for:".red(), file.cyan());
-        }
-
-        println!(
-            "({})\t{}",
-            size_to_human_readable(sizes[sizes.len() - 1] as u32),
-            file.replace(&args.root_node, "")
-        );
+    if files.len() == 0 {
+        println!("no files found");
+        return Ok(());
     }
 
-    // let (files, sizes) = process_node(&args.root_node, &args);
+    for file in files.iter() {
+        if let Ok(meta) = fs::metadata(file) {
+            #[cfg(target_os = "windows")]
+            sizes.push(meta.file_size());
+
+            #[cfg(target_os = "linux")]
+            sizes.push(meta.size());
+        } else {
+            sizes.push(0);
+            println!("{} {}", "could not read metadata for:".red(), file);
+        }
+
+        if !&args.no_list {
+            println!(
+                "({})\t{}",
+                size_to_human_readable(sizes[sizes.len() - 1] as u32),
+                file.replace(&args.root_node, "")
+            );
+        }
+    }
+
+    if !&args.no_list {
+        println!();
+    }
 
     let total_size_bytes = sizes.iter().sum::<u64>();
 
-    println!();
-    println!("total asd files: {}", sizes.len());
+    println!("🗃️ total asd files: {}", sizes.len().to_string().yellow());
     println!(
-        "total asd files size: {}",
+        "🗄️ total asd files size: {} {}",
         size_to_human_readable(total_size_bytes as u32)
+            .to_string()
+            .yellow(),
+        if args.purge {
+            "(deleted)".red()
+        } else {
+            "".white()
+        }
     );
 
-    todo!("implement purge and dry-run");
+    // todo!("implement purge and dry-run");
 
-    // if args.dry_run {
-    //     println!();
-    //     println!("files that would be removed:");
-    //     for f in files {
-    //         println!("{f}");
-    //     }
-    // }
+    if args.purge && args.dry_run {
+        println!();
+        println!("files that would be {}:", "removed".red());
+        for f in files.iter() {
+            println!("{f}");
+        }
+    }
 
     Ok(())
 }
